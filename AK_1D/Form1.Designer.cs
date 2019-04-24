fc2191dabab27828416006a578cf61519aa7e4a4
Adrian <jambondadri@gmail.com>
2019-04-24 13:13:28 +0200

    Created first project.

---
+++ AK_1D/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace AK_1D
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 23);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(2000, 1154);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2038, 634);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(2036, 421);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(6);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(240, 31);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(2038, 521);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(6);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(240, 31);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(2038, 593);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(162, 29);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Warunki Per";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2036, 390);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Szerokosc";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2036, 490);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Czas";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(2036, 319);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(238, 33);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2036, 288);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Reguła";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(2312, 1189);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximumSize = new System.Drawing.Size(2339, 1261);
+            this.MinimumSize = new System.Drawing.Size(2338, 1260);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AK_1D/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AK_1D
+{
+    public partial class Form1 : Form
+    {
+        Bitmap DrawArea;
+        Graphics g;
+        Pen mypen = new Pen(Brushes.Black);
+        Brush aBrush = (Brush)Brushes.Black;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            pictureBox1.Image = DrawArea;
+
+            comboBox1.Items.Insert(0, 30);
+            comboBox1.Items.Insert(1, 60);
+            comboBox1.Items.Insert(2, 90);
+            comboBox1.Items.Insert(3, 120);
+            comboBox1.Items.Insert(4, 225);
+            comboBox1.SelectedIndex = 0;
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            pictureBox1.Refresh();
+            g = Graphics.FromImage(DrawArea);
+            g.Clear(Color.White);
+
+            int width = Convert.ToInt32(numericUpDown1.Value);
+            int time = Convert.ToInt32(numericUpDown2.Value);
+            int selectedRule = Convert.ToInt32(comboBox1.SelectedItem);
+            int [] ruleValues = convertDecToBin(selectedRule);
+
+            int[,] tab = new int[width, time];
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < time; j++)
+                    tab[i, j] = 0;
+
+            //  Start point
+            tab[width / 2,0] = 1;
+
+            //  
+            for (int j=0; j<time-1; j++)
+            {
+                for (int i=0;i<width; i++)
+                {
+                    int a=0, b=0, c=0;
+
+                    //  Checking Periodical Conditions
+                    if (checkBox1.Checked == false)
+                    {
+                        if (i == 0)
+                        {
+                            a = 0;
+                            b = tab[i, j];
+                            c = tab[i + 1, j];
+                        }
+                        else if (i == width - 1)
+                        {
+                            a = tab[i - 1, j];
+                            b = tab[i, j];
+                            c = 0;
+                        }
+                        else
+                        {
+                            a = tab[i - 1, j];
+                            b = tab[i, j];
+                            c = tab[i + 1, j];
+                        }
+                    }
+                    else
+                    {
+                        if (i == 0)
+                        {
+                            a = tab[width - 1,j] ;
+                            b = tab[i, j];
+                            c = tab[i + 1, j];
+                        }
+                        else if (i == width - 1)
+                        {
+                            a = tab[i - 1, j];
+                            b = tab[i, j];
+                            c = tab[0,j];
+                        }
+                        else
+                        {
+                            a = tab[i - 1, j];
+                            b = tab[i, j];
+                            c = tab[i + 1, j];
+                        }
+                    }
+                    //if (check30(a,b,c) == 1) tab[i, j + 1] = 1;
+                    if (checkRule(a, b, c, ruleValues) == 1) tab[i, j + 1] = 1;
+                }
+            }
+
+            //  Visualising
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < time - 1; j++)
+                {
+                    if (tab[i, j] == 1) g.FillRectangle(aBrush, i, j, 1, 1);
+                }
+            }
+            
+
+            pictureBox1.Image = DrawArea;
+            pictureBox1.Refresh();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private int check30(int a, int b, int c)
+        {
+            if (a == 1 & b == 1 & c == 1) return 0;
+            if (a == 1 & b == 1 & c == 0) return 0;
+            if (a == 1 & b == 0 & c == 1) return 0;
+            if (a == 1 & b == 0 & c == 0) return 1;
+            if (a == 0 & b == 1 & c == 1) return 1;
+            if (a == 0 & b == 1 & c == 0) return 1;
+            if (a == 0 & b == 0 & c == 1) return 1;
+            if (a == 0 & b == 0 & c == 0) return 0;
+            return 0;
+        }
+
+        private int checkRule(int a, int b, int c, int [] values)
+        {
+            if (a == 1 & b == 1 & c == 1) return values[0];
+            if (a == 1 & b == 1 & c == 0) return values[1];
+            if (a == 1 & b == 0 & c == 1) return values[2];
+            if (a == 1 & b == 0 & c == 0) return values[3];
+            if (a == 0 & b == 1 & c == 1) return values[4];
+            if (a == 0 & b == 1 & c == 0) return values[5];
+            if (a == 0 & b == 0 & c == 1) return values[6];
+            if (a == 0 & b == 0 & c == 0) return values[7];
+            return 0;
+        }
+
+        private int [] convertDecToBin(int value)
+        {
+            string binary = Convert.ToString(value, 2).PadLeft(8, '0');
+            int [] tab1 = new int [8];
+            for (int i = 0; i < 8; i++)
+                tab1[i] = Convert.ToInt32(binary[i])-48;
+            return tab1;
+        }
+    }
+}
